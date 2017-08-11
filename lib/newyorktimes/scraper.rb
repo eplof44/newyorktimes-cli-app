3687fa760726948430989d47a7ca8d4239f657ce
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 16:56:58 +0000

    fixed scraper class to iterate through array for each story

--- lib/newyorktimes/scraper.rb
+++ lib/newyorktimes/scraper.rb
@@ -5,27 +5,22 @@ require 'pry'
 class Scraper
 
 
-
-# def self.todays_articles
-#   self.scrape_home
-# end
-#
-# def self.scrape_home
-#   articles = []
-#   articles << self.scrape_news
-#   articles
-# end
-
-  def self.scrape_news
-    doc = Nokogiri::HTML(open("https://www.nytimes.com/pages/dining/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Food&WT.nav=page"))
-    article = doc.search(".story")
-    article.each do |info|
-
-    story.headline info.search("h3").text.strip
-    story.author = info.search("h6.byline").text.strip
-    story.timestamp = info.search("h6.byline span.timestamp").text.strip
-    story.summary = info.search("p.summary").text.strip
+def get_page
+    Nokogiri::HTML(open("https://www.nytimes.com/pages/dining/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Food&WT.nav=page"))
   end
 
-end
+  def scrape_index
+    self.get_page.css("div.story")
+  end
+
+  def make_articles
+      scrape_index.each do |s|
+        headline doc.search("h3").text
+        author = doc.search("h6.byline").text.strip
+        timestamp = doc.search(".timestamp").text.strip
+        summary = doc.search("p.summary").text.strip
+        Article.new(headline, author, timestamp, summary)
+
+    end
+  end
 end
