8edaad72ebe11e6a7129904c789563d49c252a3a
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 14:57:49 +0000

    renamed article to scraper and added scraper class

---
+++ lib/newyorktimes/scraper.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+class Scraper 
+
+  attr_accessor :headline, :author, :timestamp, :summary, :url
+
+  def self.todays_articles
+    self.scrape_news
+  end
+
+
+  def self.scrape_news
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/pages/dining/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Food&WT.nav=page"))
+    article = self.new
+    article.headline = doc.search("h3").text.strip
+    article.author = doc.search("h6.byline").text.strip
+    article.timestamp = doc.search(".timestamp").text.strip
+    article.summary = doc.search("p.summary").text.strip
+    #article.url = doc.search("h2.story a").attr("href")
+    article
+  end
+
+end
