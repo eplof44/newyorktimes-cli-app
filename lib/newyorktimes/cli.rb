34e4b2af72b85869d9b0a40bccaf5daf50aecd4f
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 13:26:36 +0000

    added more helper methods to the CLI

--- lib/newyorktimes/cli.rb
+++ lib/newyorktimes/cli.rb
@@ -11,13 +11,22 @@ class CLI
 
   def welcome
     puts "Welcome to the New York Times Top News Reader!"
-    puts "Please enter the number of the article you would like to learn more about."
+    puts "Please enter the number of the article you would like to learn more about. (or type exit to quit)"
+  end
+
+  def list_articles
+    puts "here is where the articles will be listed"
   end
 
   def menu
+    list_articles
+    input = gets.strip
+    case input
+    when "1"
+      puts "show article summary"
   end
 
   def goodbye
   end
-  
+
 end
