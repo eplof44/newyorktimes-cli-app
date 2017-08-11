22575cbfa620610cc620896c5b4e85d33479c0e0
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 13:52:41 +0000

    added scraper elements to test

--- lib/newyorktimes/article.rb
+++ lib/newyorktimes/article.rb
@@ -5,19 +5,21 @@ require 'pry'
 
 class Article
 
-attr_accessor :headline, :author, :timestamp, :summary, :url
+  attr_accessor :headline, :author, :timestamp, :summary, :url
 
-def self.scrape_todays_articles
+
+
+  def self.scrape_news(url)
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/"))
+    article = self.new
+    binding.pry
+    headline = doc.search("h2.story-heading").text.strip
+    author = doc.search("p.byline").text.strip
+    timestamp = doc.search("timestamp")text.strip
+    summary = doc.search("p.summary")text.strip
+    # url =
+  end
+
 end
 
-def self.scrape_news
-  doc = Nokogiri::HTML(open("https://www.nytimes.com/"))
-  article = self.new
-  headline =
-  author =
-  timestamp =
-  summary =
-  url = 
-end
-
-end
+Article.scrape_news("https://www.nytimes.com/")
