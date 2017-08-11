44bf230b4b7795d637345422a97cdc8b889aee40
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 14:08:58 +0000

    added class methods to find articles

--- lib/newyorktimes/article.rb
+++ lib/newyorktimes/article.rb
@@ -7,19 +7,25 @@ class Article
 
   attr_accessor :headline, :author, :timestamp, :summary, :url
 
+  def self.todays_articles
+    binding.pry
+    self.scrape_home
+  end
 
+  def self.scrape_home
+    articles = []
+    articles << scrape_news
+  end
 
   def self.scrape_news(url)
     doc = Nokogiri::HTML(open("https://www.nytimes.com/"))
     article = self.new
-    binding.pry
     headline = doc.search("h2.story-heading").text.strip
     author = doc.search("p.byline").text.strip
     timestamp = doc.search(".timestamp").text.strip
     summary = doc.search("p.summary").text.strip
-    # url =
+    url = doc.search("h2.story-heading a").attr("href").strip
+    article
   end
 
 end
-
-Article.scrape_news("https://www.nytimes.com/")
