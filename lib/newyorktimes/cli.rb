fa0e71bf6fdcced6fe73c062d436fade630cb4f0
Erica Plofsky <ericaplofsky@gmail.com>
2017-08-11 13:29:40 +0000

    case input added to see articles

--- lib/newyorktimes/cli.rb
+++ lib/newyorktimes/cli.rb
@@ -6,7 +6,7 @@ class CLI
   def call
     welcome
     menu
-    goodbye
+    # goodbye
   end
 
   def welcome
@@ -22,11 +22,31 @@ class CLI
     list_articles
     input = gets.strip
     case input
-    when "1"
+    when "(1..10)"
+      puts "show article summary"
+    when "2"
+      puts "show article summary"
+    when "3"
+      puts "show article summary"
+    when "4"
+      puts "show article summary"
+    when "5"
+      puts "show article summary"
+    when "6"
+      puts "show article summary"
+    when "7"
+      puts "show article summary"
+    when "8"
+      puts "show article summary"
+    when "9"
+      puts "show article summary"
+    when "10"
       puts "show article summary"
   end
 
-  def goodbye
-  end
+def goodbye
+  puts "Have a great day!"
+end
 
+end
 end
